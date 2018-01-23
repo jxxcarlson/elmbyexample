7a7c0b778b93165fb3f610e65f3048eb8e107116
James Carlson <jxxcarlson@mac.com>
2018-01-23 12:35:24 -0500

    Add Svg1

---
+++ src/svg/Svg1.elm
@@ -0,0 +1,26 @@
+module Svg1 exposing (..)
+
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
+
+main =
+    svg
+        [ viewBox "0 0 600 600" ]
+        [ background, object ]
+
+
+background =
+    rect
+        [ fill "#ddf", x "0", y "0", width "200", height "200" ]
+        []
+
+
+object =
+    circle
+        [ cx "100", cy "100", r "30", fill color, opacity "0.9" ]
+        []
+
+
+color =
+    "rgb(200,0, 0)"
