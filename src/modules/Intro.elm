e73bab5f2761b5ed2cb3963f6b7a7df6dbbd6f57
James Carlson <jxxcarlson@gmail.com>
2018-02-11 17:26:02 -0500

    Add SocialNetwork module

--- src/modules/Intro.elm
+++ src/modules/Intro.elm
@@ -2,7 +2,19 @@ module Intro exposing (..)
 
 
 type alias Person =
-    { name : String, age : Int }
+    { id: Int
+      , firstName : String
+      , lastName: String
+      , age : Int
+      , friends: List Int
+    }
+
+p1 = Person 1 "John" "Jones"   23 [2, 4]
+p2 = Person 2 "Fred" "Mayer"   21 [1, 3]
+p3 = Person 3 "Mary" "Smith"   21 [2, 4]
+p4 = Person 4 "Jane" "Freeman" 25 [1, 2, 3]
+
+people = [p1, p2, p3, p4]
 
 
 type Suit
